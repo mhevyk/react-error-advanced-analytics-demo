e688005319d706faf6ce50edd8ff1e9d4f71d356
Maksym Hevyk <maxxevyk7@gmail.com>
2025-05-05 16:50:27 +0300

    Add test CTA button

--- google-analytics-demo/src/pages/HomePage.tsx
+++ google-analytics-demo/src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import useAnalyticsEventTracker from "@/hooks/useAnalyticsEventTracker";
-import { Button } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
@@ -9,18 +9,32 @@ const HomePage = () => {
     trackEvent("Click", "Buy products button clicked");
   };
 
+  const testCTAButton = () => {
+    trackEvent("Click", "Test CTA button clicked");
+  };
+
   return (
     <>
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
-      <Button
-        as={Link as any}
-        variant="primary"
-        to="/products"
-        onClick={handleProductsClick}
-      >
-        Buy products
-      </Button>
+      <Stack direction="vertical" gap={2}>
+        <Button
+          as={Link as any}
+          variant="primary"
+          to="/products"
+          onClick={handleProductsClick}
+          className="align-self-start"
+        >
+          Buy products
+        </Button>
+        <Button
+          variant="warning"
+          onClick={testCTAButton}
+          className="align-self-start"
+        >
+          Test CTA Button
+        </Button>
+      </Stack>
     </>
   );
 };
